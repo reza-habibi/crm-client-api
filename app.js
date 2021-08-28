7a94154407f39bfd7a0ec40f31d4aac9be02b133
reza-habibi <rezhabibi72@gmail.com>
2021-08-28 15:33:28 +0430

    create api routers

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+// API security
+app.use(helmet());
+
+// handle CORS error
+app.use(cors());
+
+// Logger
+app.use(morgan("tiny"));
+
+// Set Body Parser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+const port = process.env.Port || 4030;
+
+//load routers
+const userRouter = require("./src/routers/userRouter.js");
+const ticketRouter = require("./src/routers/ticketRouter.js");
+
+// use Routers
+
+app.use("/v1/user", userRouter);
+app.use("/v1/ticket", ticketRouter);
+
+// Error handler
+const handleError = require("./src/utils/errorHandler.js");
+
+app.use((req, res, next) => {
+  const error = new Error("هیچ دیتایی یافت نشد");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+    handleError(error , res)
+});
+
+app.listen(port, () => {
+  console.log(`api is ready on http://localhost:${port}`);
+});
---
+++ src/routers/userRouter.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const router = express.Router()
+
+
+router.all('/' , (req,res,next)=>{
+    res.json({message:'return from user router'})
+})
+
+module.exports = router;
